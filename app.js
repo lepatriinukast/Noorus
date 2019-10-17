36899668fb9191f3d72958ac5216b5a12efdde14
Joosep Trumm <joosep_trumm@hotmail.com>
2019-10-17 22:23:16 +0300

    add logo on navigation

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
 const app = express();
+
 
 
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/public/index.html");
+  res.render("avaleht");
+});
+
+app.get("/koorist", function(req, res) {
+  res.render("koorist");
 });
 
 app.listen(3000, function() {
